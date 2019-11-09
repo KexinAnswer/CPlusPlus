4840b610318ca0e4c16db49e91359fac3dd58d7e
KexinAnswer <925433202@qq.com>
2019-11-09 23:21:29 +0800

    双向队列加权值

--- 11-3/11-3/main.cpp
+++ 11-3/11-3/main.cpp
@@ -5,7 +5,7 @@
 	编写程序输入一个任意字符串，判断该字符串是否至少包含
 	大写字母，小写字母，数字三种类型字符串的两类
 */
-#if 0
+#if 1
 bool CheckPassword(const char* str, int len) {
 	// 是否有数组
 	int num = 0;
@@ -41,5 +41,40 @@ int main() {
 	return 0;
 }
 #endif
+
+
+#include <cstdio>
+#define maxsize 100
+
+// arr:双向队列  begin:队列头   end:队列尾
+int WOfQ(int arr[], int begin, int end) {
+	int sum = 0;
+	int i = 0; // i 表示第几次出队
+	while (begin <= end) {
+		++i;
+		if (arr[begin] < arr[end]) {
+			// 如果队头元素小于队尾元素 出队头元素
+			sum += arr[begin] * i;
+			++begin;
+		}
+		else {
+			// 如果队尾元素小 那么出队尾元素 
+			sum += arr[end] * i;
+			--end;
+		}
+	}
+	return sum;
+}
+
+
+int main() {
+	int arr[] = { 3,1,7,9,10,2,6 };
+	int len = sizeof(arr) / sizeof(arr[0]);
+	int sum = WOfQ(arr, 0, len - 1);
+	printf("%d\n", sum);
+	return 0;
+}
+
+
 
 
